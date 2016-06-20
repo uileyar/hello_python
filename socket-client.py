e1761fe877f46f70eba32304f2caee1011eb44a0
john <john@chinawebeye.com>
2016-06-20 19:59:48 +0800

    add epoll

--- socket-client.py
+++ socket-client.py
@@ -2,13 +2,14 @@
 # coding:utf-8
 
 import socket  # socket模块
+import time
 from util import *
 
 HOST = '0.0.0.0'
 PORT = 8888
 
 
-def main():
+def main1():
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # 定义socket类型，网络通信，TCP
     s.connect((HOST, PORT))  # 要连接的IP与端口
     while True:
@@ -18,6 +19,23 @@ def main():
         print data  # 输出变量
     s.close()  # 关闭连接
 
+def main():
+    try:
+        connFd = socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0)
+        connFd.connect((HOST, PORT))
+        logging.info("connect to network server success")
+    except socket.error, msg:
+        logging.error(msg)
 
+    for i in range(1, 11):
+        data = "The Number is %d" % i
+        if connFd.send(data) != len(data):
+            logging.error("send data to network server failed")
+            break
+        readData = connFd.recv(1024)
+        print readData
+        time.sleep(1)
+
+    connFd.close()
 if __name__ == '__main__':
     main()
--- socket-server.py
+++ socket-server.py
@@ -2,6 +2,7 @@
 # coding:utf-8
 
 import socket  # socket模块
+import select, errno
 import commands  # 执行系统命令模块
 from util import *
 
@@ -9,7 +10,7 @@ HOST = '0.0.0.0'
 PORT = 8888
 
 
-def main():
+def main1():
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # 定义socket类型，网络通信，TCP
     s.bind((HOST, PORT))  # 套接字绑定的IP与端口
     s.listen(1)  # 开始TCP监听
@@ -24,6 +25,75 @@ def main():
             else:
                 conn.sendall(cmd_result)  # 否则就把结果发给对端（即客户端）
         conn.close()  # 关闭连接
+
+
+def main():
+    try:
+        listen_fd = socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0)
+        listen_fd.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        listen_fd.bind((HOST, PORT))
+        listen_fd.listen(10)
+    except socket.error, msg:
+        logging.error("msg=%s", msg)
+        return
+
+    try:
+        epoll_fd = select.epoll()
+        epoll_fd.register(listen_fd.fileno(), select.EPOLLIN)
+    except select.error, msg:
+        logging.error(msg)
+        return
+
+    connections = {}
+    addresses = {}
+    datalist = {}
+    while True:
+        epoll_list = epoll_fd.poll()
+        for fd, events in epoll_list:
+            if fd == listen_fd.fileno():
+                conn, addr = listen_fd.accept()
+                logging.info("accept connection from %s, %d, fd = %d" % (addr[0], addr[1], conn.fileno()))
+                conn.setblocking(0)
+                epoll_fd.register(conn.fileno(), select.EPOLLIN | select.EPOLLET)
+                connections[conn.fileno()] = conn
+                addresses[conn.fileno()] = addr
+            elif select.EPOLLIN & events:
+                datas = ''
+                while True:
+                    try:
+                        data = connections[fd].recv(10)
+                        if not data and not datas:
+                            epoll_fd.unregister(fd)
+                            connections[fd].close()
+                            logging.info("%s, %d closed" % (addresses[fd][0], addresses[fd][1]))
+                            break
+                        else:
+                            datas += data
+                    except socket.error, msg:
+                        if msg.errno == errno.EAGAIN:
+                            logging.info("%s receive %s" % (fd, datas))
+                            datalist[fd] = datas
+                            epoll_fd.modify(fd, select.EPOLLET | select.EPOLLOUT)
+                            break
+                        else:
+                            epoll_fd.unregister(fd)
+                            connections[fd].close()
+                            logging.error(msg)
+                            break
+            elif select.EPOLLHUP & events:
+                epoll_fd.unregister(fd)
+                connections[fd].close()
+                logging.info("%s, %d closed" % (addresses[fd][0], addresses[fd][1]))
+            elif select.EPOLLOUT & events:
+                sendLen = 0
+                while True:
+                    sendLen += connections[fd].send(datalist[fd][sendLen:])
+                    if sendLen == len(datalist[fd]):
+                        break
+                epoll_fd.modify(fd, select.EPOLLIN | select.EPOLLET)
+            else:
+                continue
+
 
 if __name__ == '__main__':
     main()

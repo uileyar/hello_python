c0961cdd16298fb31559826b2e20eeada90a49f3
john <john@chinawebeye.com>
2016-06-21 19:19:51 +0800

    add 2

--- socket-client.py
+++ socket-client.py
@@ -9,33 +9,23 @@ HOST = '0.0.0.0'
 PORT = 8888
 
 
-def main1():
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # 定义socket类型，网络通信，TCP
-    s.connect((HOST, PORT))  # 要连接的IP与端口
+def main():
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0)
+        s.connect((HOST, PORT))
+        print ("connect to network server success")
+    except socket.error, msg:
+        print ('error=', msg)
+
     while True:
         cmd = raw_input("Please input cmd:")  # 与人交互，输入命令
+        if cmd == 'exit':
+            break
         s.sendall(cmd)  # 把命令发送给对端
         data = s.recv(1024)  # 把接收的数据定义为变量
         print data  # 输出变量
     s.close()  # 关闭连接
 
-def main():
-    try:
-        connFd = socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0)
-        connFd.connect((HOST, PORT))
-        logging.info("connect to network server success")
-    except socket.error, msg:
-        logging.error(msg)
 
-    for i in range(1, 11):
-        data = "The Number is %d" % i
-        if connFd.send(data) != len(data):
-            logging.error("send data to network server failed")
-            break
-        readData = connFd.recv(1024)
-        print readData
-        time.sleep(1)
-
-    connFd.close()
 if __name__ == '__main__':
     main()
--- socket-server.py
+++ socket-server.py
@@ -33,15 +33,10 @@ def main():
         listen_fd.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         listen_fd.bind((HOST, PORT))
         listen_fd.listen(10)
-    except socket.error, msg:
-        logging.error("msg=%s", msg)
-        return
-
-    try:
         epoll_fd = select.epoll()
         epoll_fd.register(listen_fd.fileno(), select.EPOLLIN)
-    except select.error, msg:
-        logging.error(msg)
+    except socket.error, msg:
+        logging.error("msg=%s", msg)
         return
 
     connections = {}
@@ -61,7 +56,7 @@ def main():
                 datas = ''
                 while True:
                     try:
-                        data = connections[fd].recv(10)
+                        data = connections[fd].recv(1024)
                         if not data and not datas:
                             epoll_fd.unregister(fd)
                             connections[fd].close()
@@ -86,14 +81,21 @@ def main():
                 logging.info("%s, %d closed" % (addresses[fd][0], addresses[fd][1]))
             elif select.EPOLLOUT & events:
                 sendLen = 0
-                while True:
-                    sendLen += connections[fd].send(datalist[fd][sendLen:])
-                    if sendLen == len(datalist[fd]):
-                        break
+                logging.info("cmd: %s" % datalist[fd])
+                cmd_status, cmd_result = commands.getstatusoutput(datalist[fd])  # commands.getstatusoutput执行系统命令（即shell命令），返回两个结果，第一个是状态，成功则为0，第二个是执行成功或失败的输出信息
+                if len(cmd_result.strip()) == 0:  # 如果输出结果长度为0，则告诉客户端完成。此用法针对于创建文件或目录，创建成功不会有输出信息
+                    connections[fd].sendall('Done.')
+                else:
+                    #connections[fd].sendall(cmd_result)  # 否则就把结果发给对端（即客户端）
+                    while True:
+                        sendLen += connections[fd].send(cmd_result[sendLen:])
+                        if sendLen == len(cmd_result):
+                            break
                 epoll_fd.modify(fd, select.EPOLLIN | select.EPOLLET)
             else:
                 continue
 
 
 if __name__ == '__main__':
+    logging.config.fileConfig(os.path.join(os.path.dirname(__file__), './config/logging.ini'))
     main()
